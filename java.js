47055a021b41f5eb8ba8f03d96e17897a44964ad
eliseo <elromero.wdf15@bootcamp.ucamp.io>
2023-04-23 17:18:52 -0600

    ejercicios de prueva

---
+++ java.js
@@ -0,0 +1,17 @@
+//Ejercicio 1: Escribe un programa de una sola línea que haga que aparezca en la pantalla un que diga "Hello World".alert
+// alert("Hola mundo");
+
+//Ejercicio 2: Escribe un programa de una sola línea que escriba en la pantalla un texto que diga "Hello World" (document.write).
+// document.write("Hola mundo");
+
+//Ejercicio 3: Escribe un programa de una sola línea que escriba en la pantalla el resultado de sumar 3 + 5.
+// document.write(3+5);
+
+//Ejercicio 4: Escribe un programa de dos líneas que pida el nombre del usuario con un y escriba un texto que diga "Hola nombreUsuario"prompt
+// var usuario = prompt("Ingresa tu nombre de usuario: ");
+// document.write("Hola " + usuario);
+
+//Ejercicio 5: Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+var num1 = parseInt(prompt("ingresa el primer valor: "));
+var num2 = parseInt(prompt("ingresa el segundo valor: "));
+document.write(num1+num2);
